23e6ade8bfd5c508e0544ef5d7d2e0e5b7ac4a08
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-04-28 18:09:06 +0530

    deploying backend

--- backend/src/controller/flightController.js
+++ backend/src/controller/flightController.js
@@ -15,24 +15,32 @@ router.post("", async (req, res) => {
 
 router.get("", async (req, res) => {
   try {
-    const flight = await Flight.find().lean().exec();
-    return res.status(200).send(flight);
+    // const query = { start: req.query.start, end: req.query.end };
+    if (req.query.start&&req.query.end) {
+      const flight = await Flight.find({ start: req.query.start, end: req.query.end }).lean().exec();
+      return res.status(200).send(flight);
+    } else {
+      const flight = await Flight.find().lean().exec();
+      return res.status(200).send(flight);
+    }
   } catch (error) {
     return res.status(500).send(error.message);
   }
 });
 
-router.get("/:start&end", async (req, res) => {
-  try {
-    const flight = await Flight.find({$and:[
-        {start:req.query.start},
-        {end:req.query.end}
-      ]})
-      .lean()
-      .exec();
-    return res.status(200).send(flight);
-  } catch (error) {
-    return res.status(500).send(error.message);
-  }
-});
+// router.get("", async (req, res) => {
+//   try {
+//     const flight = await Flight.find({
+//       // $and:[
+//       start: req.query.start,
+//       // req.query.end
+//       // ]
+//     })
+//       .lean()
+//       .exec();
+//     return res.status(200).send(flight);
+//   } catch (error) {
+//     return res.status(500).send(error.message);
+//   }
+// });
 module.exports = router;
