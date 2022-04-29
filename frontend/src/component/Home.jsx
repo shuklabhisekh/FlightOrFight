ee449a526ded2d7dac8e52c458ee7404a3bc9f99
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-04-29 08:42:10 +0530

    working on redux

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,10 +1,29 @@
-
-
-export const Home=()=>{
-
-    return(
-        <div>
-            Home
-        </div>
-    )
-}
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./Home.css";
+export const Home = () => {
+  return (
+    <div>
+      <div style={{ margin: "30px 0px" }}>
+        <TextField id="outlined-name" label="FROM" />
+        <br />
+        <br />
+        <TextField id="outlined-name" label="TO" />
+        <br />
+        <br />
+        <Button variant="contained">SEARCH</Button>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>AIRPORT</th>
+              <th>FROM</th>
+              <th>TO</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
+};
